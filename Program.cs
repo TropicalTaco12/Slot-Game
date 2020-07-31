ae86e925576788e27eb54d33ad21a0318f8af208
Aaron <aling0703@gmail.com>
2020-07-31 12:53:04 -0700

    Finished?

--- Program.cs
+++ Program.cs
@@ -6,11 +6,11 @@ namespace Slot_Game
     {
         protected static int origRow;
         protected static int origCol;
-        protected static void WriteAt(string s, int x, int y)
+        protected static void WriteAt(string s, int col, int row)
         {
             try
             {
-                Console.SetCursorPosition(origCol + x, origRow + y);
+                Console.SetCursorPosition(origCol + col, origRow + row);
                 Console.WriteLine(s);
             }
             catch (ArgumentOutOfRangeException e)
@@ -21,7 +21,8 @@ namespace Slot_Game
         }
         static void Main(string[] args)
         {
-
+            origRow = 1;
+            origCol = 0;
             Console.WriteLine("Welcome to the slot machine!");
             getplayerBet();
             slots();
@@ -42,7 +43,7 @@ namespace Slot_Game
             }
 
             totalChips = totalChips - currentBet;
-
+            //WORK IN PROGRESS
 
 
         }
@@ -53,9 +54,6 @@ namespace Slot_Game
 
             while (true)
             {
-
-
-
                 String space = "Press Space to Spin.";
 
                 String spinningText = "Spinning...         ";
@@ -64,6 +62,8 @@ namespace Slot_Game
                 ConsoleKeyInfo info = Console.ReadKey();
                 if (info.Key == ConsoleKey.Spacebar)
                 {
+
+
 
                     WriteAt(spinningText, 0, 3);
 
@@ -94,407 +94,153 @@ namespace Slot_Game
                     slotRow3[1] = slotNumber8;
                     slotRow3[2] = slotNumber9;
 
-                    for (int i = 0; i < 20; i++)
+                    for (int i = 0; i < 10; i++)
                     {
-
                         slotNumber = r.Next(1, 6);
-                        slotNumber2 = r.Next(1, 6);
-                        slotNumber3 = r.Next(1, 6);
-                        slotNumber4 = r.Next(1, 6);
-                        slotNumber5 = r.Next(1, 6);
-                        slotNumber6 = r.Next(1, 6);
-                        slotNumber7 = r.Next(1, 6);
-                        slotNumber8 = r.Next(1, 6);
-                        slotNumber9 = r.Next(1, 6);
 
-                        slotRow = new int[3];
-                        slotRow2 = new int[3];
-                        slotRow3 = new int[3];
 
                         slotRow[0] = slotNumber;
-                        slotRow[1] = slotNumber2;
-                        slotRow[2] = slotNumber3;
-
-                        slotRow2[0] = slotNumber4;
-                        slotRow2[1] = slotNumber5;
-                        slotRow2[2] = slotNumber6;
-
-                        slotRow3[0] = slotNumber7;
-                        slotRow3[1] = slotNumber8;
-                        slotRow3[2] = slotNumber9;
                         WriteAt(slotRow[0].ToString(), 0, 5);
-                        Thread.Sleep(50);
+                        Thread.Sleep(100);
                     }
 
-                    for (int i = 0; i < 20; i++)
+                    for (int i = 0; i < 10; i++)
                     {
 
-
-                        slotNumber = r.Next(1, 6);
-                        slotNumber2 = r.Next(1, 6);
-                        slotNumber3 = r.Next(1, 6);
                         slotNumber4 = r.Next(1, 6);
-                        slotNumber5 = r.Next(1, 6);
-                        slotNumber6 = r.Next(1, 6);
-                        slotNumber7 = r.Next(1, 6);
-                        slotNumber8 = r.Next(1, 6);
-                        slotNumber9 = r.Next(1, 6);
 
-                        slotRow = new int[3];
-                        slotRow2 = new int[3];
-                        slotRow3 = new int[3];
-
-                        slotRow[0] = slotNumber;
-                        slotRow[1] = slotNumber2;
-                        slotRow[2] = slotNumber3;
 
                         slotRow2[0] = slotNumber4;
-                        slotRow2[1] = slotNumber5;
-                        slotRow2[2] = slotNumber6;
-
-                        slotRow3[0] = slotNumber7;
-                        slotRow3[1] = slotNumber8;
-                        slotRow3[2] = slotNumber9;
                         WriteAt(slotRow2[0].ToString(), 0, 6);
-                        Thread.Sleep(50);
+                        Thread.Sleep(100);
                     }
 
-                    for (int i = 0; i < 20; i++)
+                    for (int i = 0; i < 10; i++)
                     {
 
-
-
-                        slotNumber = r.Next(1, 6);
-                        slotNumber2 = r.Next(1, 6);
-                        slotNumber3 = r.Next(1, 6);
-                        slotNumber4 = r.Next(1, 6);
-                        slotNumber5 = r.Next(1, 6);
-                        slotNumber6 = r.Next(1, 6);
                         slotNumber7 = r.Next(1, 6);
-                        slotNumber8 = r.Next(1, 6);
-                        slotNumber9 = r.Next(1, 6);
 
-                        slotRow = new int[3];
-                        slotRow2 = new int[3];
-                        slotRow3 = new int[3];
-
-                        slotRow[0] = slotNumber;
-                        slotRow[1] = slotNumber2;
-                        slotRow[2] = slotNumber3;
-
-                        slotRow2[0] = slotNumber4;
-                        slotRow2[1] = slotNumber5;
-                        slotRow2[2] = slotNumber6;
 
                         slotRow3[0] = slotNumber7;
-                        slotRow3[1] = slotNumber8;
-                        slotRow3[2] = slotNumber9;
                         WriteAt(slotRow3[0].ToString(), 0, 7);
-                        Thread.Sleep(50);
+                        Thread.Sleep(100);
                     }
 
-                    for (int i = 0; i < 20; i++)
+                    for (int i = 0; i < 10; i++)
                     {
-
-
-
                         slotNumber = r.Next(1, 6);
                         slotNumber2 = r.Next(1, 6);
-                        slotNumber3 = r.Next(1, 6);
-                        slotNumber4 = r.Next(1, 6);
-                        slotNumber5 = r.Next(1, 6);
-                        slotNumber6 = r.Next(1, 6);
-                        slotNumber7 = r.Next(1, 6);
-                        slotNumber8 = r.Next(1, 6);
-                        slotNumber9 = r.Next(1, 6);
 
-                        slotRow = new int[3];
-                        slotRow2 = new int[3];
-                        slotRow3 = new int[3];
 
-                        slotRow[0] = slotNumber;
                         slotRow[1] = slotNumber2;
-                        slotRow[2] = slotNumber3;
-
-                        slotRow2[0] = slotNumber4;
-                        slotRow2[1] = slotNumber5;
-                        slotRow2[2] = slotNumber6;
-
-                        slotRow3[0] = slotNumber7;
-                        slotRow3[1] = slotNumber8;
-                        slotRow3[2] = slotNumber9;
                         WriteAt(slotRow[1].ToString(), 3, 5);
-                        Thread.Sleep(50);
+                        Thread.Sleep(100);
                     }
-                    for (int i = 0; i < 20; i++)
-                    {
-
-
-                        slotNumber = r.Next(1, 6);
-                        slotNumber2 = r.Next(1, 6);
-                        slotNumber3 = r.Next(1, 6);
-                        slotNumber4 = r.Next(1, 6);
+                    for (int i = 0; i < 10; i++)
+                    { 
                         slotNumber5 = r.Next(1, 6);
-                        slotNumber6 = r.Next(1, 6);
-                        slotNumber7 = r.Next(1, 6);
-                        slotNumber8 = r.Next(1, 6);
-                        slotNumber9 = r.Next(1, 6);
 
-                        slotRow = new int[3];
-                        slotRow2 = new int[3];
-                        slotRow3 = new int[3];
 
-                        slotRow[0] = slotNumber;
-                        slotRow[1] = slotNumber2;
-                        slotRow[2] = slotNumber3;
-
-                        slotRow2[0] = slotNumber4;
                         slotRow2[1] = slotNumber5;
-                        slotRow2[2] = slotNumber6;
-
-                        slotRow3[0] = slotNumber7;
-                        slotRow3[1] = slotNumber8;
-                        slotRow3[2] = slotNumber9;
                         WriteAt(slotRow2[1].ToString(), 3, 6);
-                        Thread.Sleep(50);
+                        Thread.Sleep(100);
                     }
-                    for (int i = 0; i < 20; i++)
+                    for (int i = 0; i < 10; i++)
                     {
 
-
-                        slotNumber = r.Next(1, 6);
-                        slotNumber2 = r.Next(1, 6);
-                        slotNumber3 = r.Next(1, 6);
-                        slotNumber4 = r.Next(1, 6);
-                        slotNumber5 = r.Next(1, 6);
-                        slotNumber6 = r.Next(1, 6);
                         slotNumber7 = r.Next(1, 6);
-                        slotNumber8 = r.Next(1, 6);
-                        slotNumber9 = r.Next(1, 6);
 
-                        slotRow = new int[3];
-                        slotRow2 = new int[3];
-                        slotRow3 = new int[3];
 
-                        slotRow[0] = slotNumber;
-                        slotRow[1] = slotNumber2;
-                        slotRow[2] = slotNumber3;
-
-                        slotRow2[0] = slotNumber4;
-                        slotRow2[1] = slotNumber5;
-                        slotRow2[2] = slotNumber6;
-
-                        slotRow3[0] = slotNumber7;
-                        slotRow3[1] = slotNumber8;
-                        slotRow3[2] = slotNumber9;
+                        slotRow3[1] = slotNumber7;
                         WriteAt(slotRow3[1].ToString(), 3, 7);
-                        Thread.Sleep(50);
+                        Thread.Sleep(100);
                     }
 
-                    for (int i = 0; i < 20; i++)
+                    for (int i = 0; i < 10; i++)
                     {
 
-
-                        slotNumber = r.Next(1, 6);
-                        slotNumber2 = r.Next(1, 6);
                         slotNumber3 = r.Next(1, 6);
-                        slotNumber4 = r.Next(1, 6);
-                        slotNumber5 = r.Next(1, 6);
-                        slotNumber6 = r.Next(1, 6);
-                        slotNumber7 = r.Next(1, 6);
-                        slotNumber8 = r.Next(1, 6);
-                        slotNumber9 = r.Next(1, 6);
 
-                        slotRow = new int[3];
-                        slotRow2 = new int[3];
-                        slotRow3 = new int[3];
 
-                        slotRow[0] = slotNumber;
-                        slotRow[1] = slotNumber2;
+
                         slotRow[2] = slotNumber3;
-
-                        slotRow2[0] = slotNumber4;
-                        slotRow2[1] = slotNumber5;
-                        slotRow2[2] = slotNumber6;
-
-                        slotRow3[0] = slotNumber7;
-                        slotRow3[1] = slotNumber8;
-                        slotRow3[2] = slotNumber9;
                         WriteAt(slotRow[2].ToString(), 6, 5);
-                        Thread.Sleep(50);
+                        Thread.Sleep(100);
                     }
-                    for (int i = 0; i < 20; i++)
+                    for (int i = 0; i < 10; i++)
                     {
 
-
-                        slotNumber = r.Next(1, 6);
-                        slotNumber2 = r.Next(1, 6);
-                        slotNumber3 = r.Next(1, 6);
-                        slotNumber4 = r.Next(1, 6);
-                        slotNumber5 = r.Next(1, 6);
                         slotNumber6 = r.Next(1, 6);
-                        slotNumber7 = r.Next(1, 6);
-                        slotNumber8 = r.Next(1, 6);
-                        slotNumber9 = r.Next(1, 6);
 
-                        slotRow = new int[3];
-                        slotRow2 = new int[3];
-                        slotRow3 = new int[3];
 
-                        slotRow[0] = slotNumber;
-                        slotRow[1] = slotNumber2;
-                        slotRow[2] = slotNumber3;
-
-                        slotRow2[0] = slotNumber4;
-                        slotRow2[1] = slotNumber5;
                         slotRow2[2] = slotNumber6;
-
-                        slotRow3[0] = slotNumber7;
-                        slotRow3[1] = slotNumber8;
-                        slotRow3[2] = slotNumber9;
                         WriteAt(slotRow2[2].ToString(), 6, 6);
-                        Thread.Sleep(50);
+                        Thread.Sleep(100);
                     }
-                    for (int i = 0; i < 20; i++)
+                    for (int i = 0; i < 10; i++)
                     {
 
-
-                        slotNumber = r.Next(1, 6);
-                        slotNumber2 = r.Next(1, 6);
-                        slotNumber3 = r.Next(1, 6);
-                        slotNumber4 = r.Next(1, 6);
-                        slotNumber5 = r.Next(1, 6);
-                        slotNumber6 = r.Next(1, 6);
-                        slotNumber7 = r.Next(1, 6);
-                        slotNumber8 = r.Next(1, 6);
                         slotNumber9 = r.Next(1, 6);
 
-                        slotRow = new int[3];
-                        slotRow2 = new int[3];
-                        slotRow3 = new int[3];
-
-                        slotRow[0] = slotNumber;
-                        slotRow[1] = slotNumber2;
-                        slotRow[2] = slotNumber3;
-
-                        slotRow2[0] = slotNumber4;
-                        slotRow2[1] = slotNumber5;
-                        slotRow2[2] = slotNumber6;
-
-                        slotRow3[0] = slotNumber7;
-                        slotRow3[1] = slotNumber8;
                         slotRow3[2] = slotNumber9;
                         WriteAt(slotRow3[2].ToString(), 6, 7);
-                        Thread.Sleep(50);
-
+                        Thread.Sleep(100);
                     }
-
-
-
+                    WinCondition(slotRow3, slotRow2, slotRow);
                 }
 
 
-
-
-
             }
-
-
         }
-
-        private static void winMethod()
+        public static void WinMessage()
         {
-            Console.WriteLine("Yay you win");
+
+            Console.WriteLine("You Win!");
         }
-
-        private static void checkWin(int[] slotRow, int[] slotRow2, int[] slotRow3)
+        public static void WinCondition(int[] slotRow, int[] slotRow2, int[] slotRow3)
         {
-            bool slotCheck = false;
-            while (slotCheck == true)
+            //left column
+            if (slotRow[0] == slotRow2[0] && slotRow2[0] == slotRow3[0])
             {
-                slotCheck = false;
+                WriteAt("You Win! You won in the left column!", 0, 9);
+               
             }
-            if (slotRow[0] == slotRow2[0])
+            //middle column
+            else if (slotRow[1] == slotRow2[1] && slotRow2[1] == slotRow3[1])
             {
-                slotCheck = true;
+                WriteAt("You Win! You won by the middle column!", 0, 9);
+                
             }
-            if (slotCheck == true)
+            // right column
+            else if (slotRow[2] == slotRow2[2] && slotRow2[2] == slotRow3[2])
             {
-                if (slotRow2[0] == slotRow3[0])
-                {
-                    winMethod();
-                    getplayerBet();
-                }
+                WriteAt("You Win! You won by the right column!", 0, 9);
+              
             }
-            //middle vertical
-            if (slotRow[1] == slotRow2[1])
+            //middle row
+            else if (slotRow2[0] == slotRow2[1] && slotRow2[1] == slotRow2[2])
             {
-                slotCheck = true;
-            }
-            if (slotCheck == true)
-            {
-                if (slotRow2[1] == slotRow3[1])
-                {
-                    winMethod();
-                    getplayerBet();
-                }
-            }
-            //left vertical
-            if (slotRow[2] == slotRow2[2])
-            {
-                slotCheck = true;
-            }
-            if (slotCheck == true)
-            {
-                if (slotRow2[2] == slotRow3[2])
-                {
-                    winMethod();
-                    getplayerBet();
-                }
+                WriteAt("You Win! You won by the middle row!", 0, 9);
+               
             }
             //left diagonal
-            if (slotRow[0] == slotRow2[1])
+            else if (slotRow[2] == slotRow2[1] && slotRow2[1] == slotRow3[0])
             {
-                slotCheck = true;
+                WriteAt("You Win! You won by left diagonal!", 0, 9);
+               
             }
-            if (slotCheck == true)
+            else if (slotRow[0] == slotRow2[1] && slotRow2[1] == slotRow3[2])
             {
-                if (slotRow2[1] == slotRow3[2])
-                {
-                    winMethod();
-                    getplayerBet();
-                }
+                WriteAt("You Win! You won by right diagonal!", 0, 9);
+                
             }
-            //right diagonal
-            if (slotRow[2] == slotRow2[1])
+            else
             {
-                slotCheck = true;
+                Console.WriteLine("You lose!");
             }
-            if (slotCheck == true)
-            {
-                if (slotRow2[1] == slotRow3[0])
-                {
-                    winMethod();
-                    getplayerBet();
-                }
-
-            }
-
-
         }
-
-        public string Replace(string Space, string spinningText)
-        {
-            spinningText = Space.Replace("Press Space to Spin", "Spinning...");
-
-            return spinningText;
-        }
-        private static void loseCondition()
-        {
-            Console.WriteLine("AW, YOU LOST!!!!!!");
-        }
-
-
+       
 
     }
 }
+
